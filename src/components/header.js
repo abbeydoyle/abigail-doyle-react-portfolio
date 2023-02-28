e7a0b0f02a2c85628f0a52125e632c317413a7d0
Abigail Doyle <abigaildoyle3@gmail.com>
2023-02-27 22:12:02 -0800

    header

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// TODO: styles
+
+const styles = {
+
+};
+
+const Header = ({ currentPage, handlePageChange }) => {
+      return (
+            <Navbar expand="sm" style={styles.heading}>
+                  <Navbar.Brand>
+                        Abigail Doyle
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                              <Nav.Link href="#about" onClick={() => handlePageChange("About")} className={currentPage === "About" ? "nav-link active" : "nav-link"}>
+                                    About Me
+                              </Nav.Link>
+                              <Nav.Link href="#resume" onClick={() => handlePageChange("Resume")} className={currentPage === "Resume" ? "nav-link active" : "nav-link"}>
+                                    Resume
+                              </Nav.Link>
+                              <Nav.Link href="#portfolio" onClick={() => handlePageChange("Portfolio")} className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>
+                                    Portfolio
+                              </Nav.Link>
+                              <Nav.Link href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>
+                                    Contact
+                              </Nav.Link>
+                        </Nav>
+                  </Navbar.Collapse>
+          </Navbar>
+      )
+}
+
+export default Header
