3ef3045894fb42f4232511d7a6fbac4e1f8c87cb
Abigail Doyle <abigaildoyle3@gmail.com>
2023-02-28 09:04:46 -0800

    placeholder about me

---
+++ src/components/SubPages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import placeholder from "../../assets/placeholder.png"
+
+// TODO: styling
+
+// TODO: finish
+
+export default function About() {
+      return (
+            <div>About Me</div>
+      )
+}
